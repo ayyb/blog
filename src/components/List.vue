a01e1e8bef17215fe0b4932778545e1da6315983
ayyb <ayyb@kakao.com>
2021-12-02 15:08:39 +0900

    개인블로그 생성 및 초기화(부트스트랩 설정)

---
+++ src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <div v-for="(item,index) in dataList" :key="index">
+    <h5>{{item.title}}</h5>
+    <p>{{item.date}}</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    name : 'List',
+    props:{
+        dataList:Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
